79ce0207c9c7af3efd00087afe76241ff3391318
Avinash219 <avinash.dev21987@gmail.com>
2020-07-19 16:19:31 +0530

    Configuration

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/readersFest", { useNewUrlParser: true })
+mongoose.connect("mongodb+srv://admin:COVNALLY1@readersfest.9qald.mongodb.net/readersfest?retryWrites=true&w=majority", { useNewUrlParser: true })
     .then(() => console.log("Database is connected"))
     .catch((err) => console.log(err))
 
